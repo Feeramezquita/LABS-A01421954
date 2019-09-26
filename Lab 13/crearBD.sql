bd1890506cc0fabd3c6165bcb40d59945d53ee9f
Fernando Amezquita <feramezquita@outlook.com>
2019-09-26 10:26:02 -0500

    terminar lab SQL

--- Lab 13/crearBD.sql
+++ Lab 13/crearBD.sql
@@ -1,3 +1,5 @@
+/*Ejercicio 1*/
+
 /* Borrar tablas*/
 drop TABLE Entregan;
 drop TABLE Materiales;
@@ -32,6 +34,7 @@ CREATE TABLE Entregan
   Cantidad numeric (8,2)
 )
 
+/*Ejercicio 2*/
 INSERT INTO Materiales values(1000, 'xxx', 1000)
 Delete from Materiales where Clave = 1000 and Costo = 1000
 
@@ -41,4 +44,24 @@ INSERT INTO Materiales values(1000, 'xxx', 1000)
 
 sp_helpconstraint materiales
 
-ALTER TABLE Materiales add constraint llaveMateriales PRIMARY KEY (Clave)
+ALTER TABLE Proveedores add constraint llaveProveedores PRIMARY KEY (RFC)
+
+ALTER TABLE Proyectos add constraint llaveProyectos PRIMARY KEY (Numero)
+
+ALTER TABLE Entregan add constraint llaveEntregan PRIMARY KEY (Clave, RFC, Numero, Fecha)
+
+/*Ejercicio 3*/
+
+SELECT *from Entregan;
+
+
+INSERT INTO entregan values (0, 'xxx', 0, '1-jan-02', 0);
+
+Delete from Entregan where Clave = 0
+
+ALTER TABLE entregan add constraint cfentreganclave
+foreign key (clave) references materiales(clave);
+
+/*Ejercicio 4*/
+
+  INSERT INTO entregan values (1000, 'AAAA800101', 5000, GETDATE(), 0);
