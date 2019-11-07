792742f8432a105afe6fae4a3b16a3aa5fafc166
Fernando Amezquita <feramezquita@outlook.com>
2019-11-06 19:06:42 -0600

    Ejemplo de Rollback

---
+++ Lab 24/insertarError.sql
@@ -0,0 +1,15 @@
+BEGIN;
+
+INSERT INTO  clienteBanca(nocuenta,nombre,saldo) VALUES 
+(00001, 'Santi', 4700),
+(00002, 'Fer', 2424),
+(00003, 'Juan', 5657),
+(00004, 'Sebas', 3895);
+
+INSERT INTO tipoMovimiento(clavem, descripcion) VALUES 
+('m1', 'Deposito'),
+('m2', 'Retiro'),
+('m3', 'Consulta de Saldo');
+-- PostgreSQL automaticamente hace ROLLBACK si hubo un error.
+END; -- Lo mismo que COMMIT.
+
